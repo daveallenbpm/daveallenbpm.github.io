da5f20bd83002f87a4e89ee57a3c0cbae681cb65
Dave Allen <daveallenbpm@gmail.com>
2018-10-09 17:56:29 +0100

    Just make the thing a link for now. Actually a way better user experience!

--- js/itch-game-loader.js
+++ js/itch-game-loader.js
@@ -1,12 +1,14 @@
-var snakeButton = document.getElementById("snake-load-button");
-var gamePreloadContainer = document.getElementById("game-preload-container");
-var snakeIFrame = document.getElementById("snake-iframe");
-var snakeIFrameContainer = document.getElementById("snake-iframe-container");
+(function(){
+    var snakeButton = document.getElementById("snake-load-button");
+    var gamePreloadContainer = document.getElementById("game-preload-container");
+    var snakeIFrame = document.getElementById("snake-iframe");
+    var snakeIFrameContainer = document.getElementById("snake-iframe-container");
 
-function snakeButtonClick() {
-    snakeIFrame.src = "https://itch.io/embed-upload/1083437?color=333333";
-    snakeIFrameContainer.style.display = "block";
-    gamePreloadContainer.style.display = "none";
-}
+    function snakeButtonClick() {
+        snakeIFrame.src = "https://itch.io/embed-upload/1083437?color=333333";
+        snakeIFrameContainer.style.display = "block";
+        gamePreloadContainer.style.display = "none";
+    }
 
-snakeButton.addEventListener("click", snakeButtonClick);
+    //snakeButton.addEventListener("click", snakeButtonClick);
+})();
